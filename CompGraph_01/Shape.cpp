1f8a7799ac3c7c602173b76f099da55bad950c55
Marcelo <mitconenp@gmail.com>
2018-09-10 20:16:06 -0300

    adicionar arestas, estrutura de vertice, print a partir das arestas

--- CompGraph_01/Shape.cpp
+++ CompGraph_01/Shape.cpp
@@ -1,26 +1,40 @@
 #include "stdafx.h"
 #include "Shape.h"
 
+#include "iostream"
+
+
+Vertex::Vertex(int mX, int mY) : x(mX), y(mY), z(0), scale(1) {}
+
+int Vertex::operator[](int i) {
+	switch (i)
+	{
+	case 0: return x;
+	case 1: return y;
+	case 2: return z;
+	case 3: return scale;
+	default:
+		return -1;
+	}
+}
 
 Shape::Shape()
 {
 	v = 0;
 	e = 0;
 	f = 0;
-	delete[] vertices;
-	vertices = NULL;
 }
 
 int Shape::x(int n)
 {
 	if (v <= n) return -1;
-	return vertices[n * 4] * vertices[n * 4 + 3];
+	return vertices[n].x * vertices[n].scale;
 }
 
 int Shape::y(int n)
 {
 	if (n >= v) return -1;
-	return vertices[n * 4 + 1] * vertices[n * 4 + 3];
+	return vertices[n].y * vertices[n].scale;
 }
 
 int Shape::getV()
@@ -31,44 +45,42 @@ int Shape::getV()
 void Shape::scale(int n)
 {
 	for (int i = 0; i < v; i++) {
-		vertices[i * 4 + 3] = n;
+		vertices[i].scale = n;
 	}
 }
 
 void Shape::addVertex(int x, int y)
 {
-	if (!vertices) {
-		vertices = new int[4]{ x, y, 0, 1 };
-		v = 1;
-	}
-	else {
-		int *aux = vertices;
-		vertices = new int[4 * (v + 1)];
-		for (int i = 0; i < v * 4; i++) {
-			vertices[i] = aux[i];
-		}
-		delete[] aux;
-		vertices[v * 4] = x;
-		vertices[v * 4 + 1] = y;
-		vertices[v * 4 + 2] = 0;
-		vertices[v * 4 + 3] = 1;
-		v++;
-	}
+	vertices.push_back(Vertex(x, y));
+	v++;
 }
+
 
 void Shape::move(int x, int y)
 {
 	for (int i = 0; i < v; i++) {
-		vertices[i * 4] += x;
-		vertices[i * 4 + 1] += y;
+		vertices[i].x += x;
+		vertices[i].y += y;
 	}
 }
 
 void Shape::printShape(HDC hdc)
 {
-	MoveToEx(hdc, this->x(0), this->y(0), NULL);
-	for (int i = 1; i < this->getV(); i++) {
-		LineTo(hdc, this->x(i), this->y(i));
+	//MoveToEx(hdc, this->x(0), this->y(0), NULL);
+	
+	//for (int i = 1; i < this->getV(); i++) {
+	//	LineTo(hdc, this->x(i), this->y(i));
+	//}
+	//LineTo(hdc, this->x(0), this->y(0));
+	for (auto edge = edges.begin(); edge != edges.end(); ++edge) {
+		MoveToEx(hdc, this->x(edge->first), this->y(edge->first), NULL);
+		LineTo(hdc, this->x(edge->second), this->y(edge->second));
 	}
-	LineTo(hdc, this->x(0), this->y(0));
+}
+
+void Shape::addEdge(int v1, int v2)
+{
+	if (v <= v1 || v <= v2) return;
+	edges.push_back(pair<int, int>(v1, v2));
+	e++;
 }
--- CompGraph_01/Shape.h
+++ CompGraph_01/Shape.h
@@ -1,8 +1,25 @@
 #pragma once
 #include <windef.h>
+#include <vector>
+
+using namespace std;
+
+struct Vertex
+{
+	int x = 0;
+	int y = 0;
+	int z = 0;
+	int scale = 1;
+
+	Vertex(int mX, int mY);
+	int operator[](int i);
+};
+
 class Shape
 {
-	int *vertices = NULL, *edges = NULL, *faces = NULL;
+	int *faces = NULL;
+	vector<Vertex> vertices;
+	vector<pair<int, int>> edges;
 	//number of elements on the above arrays
 	int v = 0, e = 0, f = 0;
 
@@ -17,4 +34,5 @@ class Shape
 		void addVertex(int x, int y);
 		void move(int x, int y);
 		void printShape(HDC hdc);
+		void addEdge(int v1, int v2);
 };
